92c4f5bab0bc94ff00a973a336d26815ebd3b4fd
picojazz <picojazzz@gmail.com>
2017-10-05 11:53:25 +0000

    first commit

---
+++ src/main/java/com/example/crudspringmvc/CrudspringmvcApplication.java
@@ -0,0 +1,12 @@
+package com.example.crudspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudspringmvcApplication.class, args);
+	}
+}
