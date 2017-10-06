5e00524f6f10c4b43284d8fc80e703c68a0c3977
picojazz <picojazzz@gmail.com>
2017-10-06 14:48:40 +0000

    finish paginate and add delete action with flash message

--- src/main/java/com/example/crudspringmvc/web/ProduitController.java
+++ src/main/java/com/example/crudspringmvc/web/ProduitController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ProduitController {
@@ -19,15 +20,27 @@ public class ProduitController {
     @RequestMapping(value = "/all")
     public String index(Model model , @RequestParam(name = "motcle",defaultValue = "")String mc,
                         @RequestParam(name = "page",defaultValue = "0")int p,
-                        @RequestParam(name = "size",defaultValue = "10") int s){
+                        @RequestParam(name = "size",defaultValue = "8") int s){
 
         Page<Produit> produits = pr.rechParMc("%"+mc+"%",new PageRequest(p,s));
+        int[] pageTotals = new int[produits.getTotalPages()];
 
         model.addAttribute("produits",produits);
-    model.addAttribute("mc",mc);
+        model.addAttribute("mc",mc);
+        model.addAttribute("pageTot",pageTotals);
+        model.addAttribute("pc",p);
 
     return "index";
 
+    }
+    @RequestMapping(value = "/delete")
+    public String delete(Model model , RedirectAttributes redirectAttributes,Long id,String mc,int p){
+        pr.delete(id);
+        redirectAttributes.addFlashAttribute("type","alert alert-success");
+        redirectAttributes.addFlashAttribute("message","le produit "+id+" a bien etait supprimé !");
+
+
+        return "redirect:all?motcle="+mc+"&page="+p;
     }
 
 }
