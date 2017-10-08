03f77fb8e25704068719af7948910257c7f7bd29
picojazz <picojazzz@gmail.com>
2017-10-08 16:19:02 +0000

    add restful api for product

--- src/main/java/com/example/crudspringmvc/web/RestProduitController.java
+++ src/main/java/com/example/crudspringmvc/web/RestProduitController.java
@@ -1,4 +1,54 @@
 package com.example.crudspringmvc.web;
 
+
+import com.example.crudspringmvc.dao.ProduitRepository;
+import com.example.crudspringmvc.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
 public class RestProduitController {
+
+    @Autowired
+    private ProduitRepository pr;
+
+    @RequestMapping(value = "/produits")
+    public List<Produit> getProduits(){
+
+        return pr.findAll();
+    }
+    @RequestMapping(value = "/produits/{id}", method = RequestMethod.GET)
+    public Produit getProduit(@PathVariable(name = "id") Long id){
+
+        return pr.findOne(id);
+    }
+    @RequestMapping(value = "/produits/new", method = RequestMethod.POST)
+    public Boolean newProduit(@Valid Produit p){
+        pr.save(p);
+        return true;
+    }
+    @RequestMapping(value = "/produits/{id}", method = RequestMethod.PUT    )
+    public Boolean updateProduit(@PathVariable(name = "id") Long id,@Valid Produit p){
+        p.setId(id);
+        pr.save(p);
+        return true;
+    }
+    @RequestMapping(value = "/produits/{id}", method = RequestMethod.DELETE)
+    public Boolean deleteProduit(@PathVariable(name = "id") Long id){
+        pr.delete(id);
+        return true;
+    }
+    @RequestMapping(value = "/produits", method = RequestMethod.GET)
+    public Page<Produit> pageProduits( @RequestParam(name = "motcle",defaultValue = "")String mc,
+                                       @RequestParam(name = "page",defaultValue = "0")int p,
+                                       @RequestParam(name = "size",defaultValue = "8") int s){
+
+        return pr.rechParMc("%"+mc+"%",new PageRequest(p,s));
+    }
+
 }
