3ac7caee7cf00f8ea6ca7deb97afe1edb5f90f5d
picojazz <picojazzz@gmail.com>
2017-10-23 16:17:09 +0000

    add product card

--- src/main/java/com/example/crudspringmvc/web/FrontController.java
+++ src/main/java/com/example/crudspringmvc/web/FrontController.java
@@ -1,14 +1,20 @@
 package com.example.crudspringmvc.web;
 
 
+import com.example.crudspringmvc.dao.ProduitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontController {
+    @Autowired
+    private ProduitRepository pr;
 
     @RequestMapping(value = "/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("produits",pr.findAll());
         return "front/index";
     }
 }
