6b595788716dbce19502ce2de8b3a980c9ccf68b
picojazz <picojazzz@gmail.com>
2017-10-25 17:16:44 +0000

    add details product

--- src/main/java/com/example/crudspringmvc/web/FrontController.java
+++ src/main/java/com/example/crudspringmvc/web/FrontController.java
@@ -2,10 +2,15 @@ package com.example.crudspringmvc.web;
 
 
 import com.example.crudspringmvc.dao.ProduitRepository;
+import com.example.crudspringmvc.entities.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FrontController {
@@ -18,9 +23,25 @@ public class FrontController {
         return "front/index";
     }
     @RequestMapping(value = "/product-all")
-    public String product(Model model){
+    public String product(Model model , @RequestParam(name = "motcle",defaultValue = "")String mc,
+                          @RequestParam(name = "page",defaultValue = "0")int p,
+                          @RequestParam(name = "size",defaultValue = "8") int s){
 
-        model.addAttribute("produits",pr.findAll());
+        Page<Produit> produits = pr.rechParMc("%"+mc+"%",new PageRequest(p,s));
+        int[] pageTotals = new int[produits.getTotalPages()];
+
+        model.addAttribute("produits",produits);
+        model.addAttribute("mc",mc);
+        model.addAttribute("pageTot",pageTotals);
+        model.addAttribute("pc",p);
         return "front/product-all";
+    }
+
+    @RequestMapping(value = "/product-all/{id}")
+    public String details(Model model, @PathVariable("id") Long id){
+
+        model.addAttribute("produit",pr.findOne(id));
+
+        return "front/details";
     }
 }
